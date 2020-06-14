e21bfa9b1d6893ea3e4a190e76017a0e032aac04
kholilboy <kholil.zboy@gmail.com>
2020-06-14 21:11:25 +0700

    add master blade

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">Home</a>
+            <a href="/dashboard">Blog</a>
+        </nav>
+    </header>
+    @yield('content')
+
+    <footer>@copy Laravel 7 | 2020</footer>
+</body>
+</html>
