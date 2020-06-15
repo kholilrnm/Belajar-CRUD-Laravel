1fafeb861dc944491bc1875ce3eca756b487d37d
kholilboy <kholil.zboy@gmail.com>
2020-06-15 23:16:22 +0700

    update show blade

--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -1,18 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.master')
+
+
+@section('content')
     <h1>Hello Kholil Blog 1</h1>
     <h2>{{ $nilai }}</h2>
     <h2>{{ $users }}</h2>
     @foreach ($dataArray as $item)
         {{ $item }}
     @endforeach
-    tes 123
-</body>
-</html>
+
+@endsection
+    
