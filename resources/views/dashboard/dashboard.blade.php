c4d274a46aec1d33e39515b3f998690cc50dd280
kholilboy <kholil.zboy@gmail.com>
2020-06-18 23:30:31 +0700

    section and yield

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,14 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-    <h1>Hello Kholil</h1>
+@extends('layouts.master')
 
-</body>
-</html>
+
+@section('title', 'Blog Sekolah Koding Awal')
+
+@section('content')
+    <h1>Hello Kholil Blog 1</h1>
+    <h2>{{ $nilai }}</h2>
+    <h2>{{ $users }}</h2>
+    @foreach ($dataArray as $item)
+        {{ $item }}
+    @endforeach
+
+    <p>Ini blog dashboard awal</p>
+
+@endsection
