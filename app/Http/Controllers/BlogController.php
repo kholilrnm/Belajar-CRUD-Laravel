796944dbadf539a3650336af3054a288f674d708
kholilboy <kholil.zboy@gmail.com>
2020-06-21 21:52:06 +0700

    database query builder

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class BlogController extends Controller
 {
@@ -14,12 +16,22 @@ class BlogController extends Controller
 
     public function show($id)
     {
+        // variabel(A) = nilai variabel(B) -> nek kate dilebokno return nak view dadi -> (C) => (A) DI DALEM blade viewnya pake echo yg (C)
         $nilai = 'ini adalah nilainya ' . $id;
         $users = "Kholilul Rachman NM";
         $dataArray = ['ini array : kholil', 'ul', 'rachman', 'nur','manab'];
         $unescaped = '<script>alert("x")</script>';
         $unescaped123 = '<script>alert("x")</script>';
+        $tes = '<br>';
+        $ambilData = DB::table('users')->get();
 
-        return view('dashboard/show', ['nilai' => $nilai, 'users' => $users, 'dataArray' => $dataArray, 'unescaped' => $unescaped, 'unescaped123' => $unescaped123]);
+        return view('dashboard/show', ['nilai' => $nilai,
+                                        'users' => $users, 
+                                        'dataArray' => $dataArray, 
+                                        'unescaped' => $unescaped, 
+                                        'unescaped123' => $unescaped123,
+                                        'getUsersDB' => $ambilData,
+                                        'tes' => $tes
+                                        ]);
     }
 }
